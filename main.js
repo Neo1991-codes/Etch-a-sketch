6670ef0a9a9e8c79f470abb5bb5194e969356428
neo siwela <nvsiwela1991@gmail.com>
2023-12-18 15:25:47 +0200

    finished the click to draw

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = "Now you can Draw";
+            } else {
+                draw.innerHTML = "You're now Allowed to Draw";
+            }
+        }
+    })
 
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
@@ -42,11 +55,13 @@ function getSize(){
 }
 
 function colorDiv() {
+    if(click){
     if(color == "random"){
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
     } else {
         this.style.backgroundColor = 'black';
     }
+ }
 }
 
 function setColor(colorChoice){
